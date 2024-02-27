163347c0ba519ea2b7e6d2f22ea19c4ff5158153
Navaneeth Nivol <33155848+navaneethnivol@users.noreply.github.com>
2024-02-26 21:46:52 -0800

    basic backend setup

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -59,7 +59,7 @@ export default function Home() {
   }
 
   const createPad = async (data: PadData) => {
-    const response = await fetch("http:localhost:3100/create", {
+    const response = await fetch("http://localhost:9000/create", {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
