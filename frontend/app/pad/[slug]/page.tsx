9cb07d753e978beb8b539b5f0d4ba34d57b8e561
“AsishRaju” <vashish888@gmail.com>
2024-03-12 15:39:12 -0700

    removed unwated imports

--- frontend/app/pad/[slug]/page.tsx
+++ frontend/app/pad/[slug]/page.tsx
@@ -1,42 +1,38 @@
-'use client'
-
-import ErrorPage from 'next/error';
-import { faCopy } from '@fortawesome/free-solid-svg-icons';
-import { toast } from 'sonner'
-
-import MyButton from '@/components/button/button';
 import { PadData } from '@/types/configBar';
+import ClipboardButton from '@/components/button/ClipboardButton';
+
+async function getPadData(slug: string) {
+    const response = await fetch(`${process.env.NEXT_PUBLIC_BASE_API_URL}/pad/${slug}`, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        cache: 'no-store'
+    });
+
+    return response
+
+}
 
 
 export default async function ViewPage(context: any) {
-
     try {
-        const response = await fetch(`${process.env.NEXT_PUBLIC_BASE_API_URL}/pad/${context.params.slug}`, {
-            method: 'GET',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            cache: 'no-store'
-        });
 
-        const data: PadData = await response.json()
+        const data: PadData = await (await getPadData(context.params.slug)).json()
 
         return (
             <section className="h-full my-8 flex flex-col justify-center">
-
                 <textarea className="rounded-[10px] w-full h-full sm:h-3/4 p-4 text-[var(--sub-color)] bg-[var(--sub-alt-color)]" value={data.input} readOnly />
-
-                <div className="flex flex-row justify-center m-8">
-                    <button onClick={async () => {
-                        await navigator.clipboard.writeText(data.input);
-                        toast.success('Pastepad copied to clipboard.')
-                    }}>
-                        <MyButton icon={faCopy} name='copy' href='' target='' className='p-2'></MyButton>
-                    </button>
-                </div>
+               <ClipboardButton data={data.input}></ClipboardButton>
             </section>
         );
     } catch (e) {
-        return <ErrorPage statusCode={404} />;
+        return <div className="flex  items-center justify-center text-white">
+        <div className="text-center">
+          <p className="text-9xl font-bold">404 <span className='font-normal'>| &nbsp; This page could not be found.</span></p>
+          
+         
+        </div>
+      </div>;
     }
 }
